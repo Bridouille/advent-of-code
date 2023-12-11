f7333914fd572fc161c32cdf29189dc5a3ad2800
Nicolas Bridoux <nbridoux@spotify.com>
2023-12-11 11:30:23 +0100

    Added day10 p2

--- 2023/src/day10/day10.kt
+++ 2023/src/day10/day10.kt
@@ -4,7 +4,6 @@ import GREEN
 import RESET
 import printTimeMillis
 import readInput
-import java.awt.Point
 
 private fun findStart(table: Array<CharArray>): List<Int> {
     for (y in table.indices) {
@@ -33,33 +32,37 @@ enum class Possibles(val letters: Set<Char>) {
     RIGHT(setOf('-', '7', 'J'))
 }
 
+data class Point(val x: Int, val y: Int)
 data class Visit(
     val x: Int,
     val y: Int,
-    val possibles: Set<Char>,
-    val step: Int
+    val possibles: Set<Char>
 )
 
-fun part1(input: List<String>): Int {
-    val table = Array(input.size) { idx -> input[idx].toCharArray() }
-    val (startX, startY) = findStart(table)
+// Traverses the map in a dfs and return the points contained in the loop
+private fun dfs(
+    table: Array<CharArray>,
+    startX: Int,
+    startY: Int
+): Set<Point> {
     val stack = mutableListOf<Visit>()
     val visited = mutableSetOf<Point>()
 
     println("Start = ($startX, $startY)")
 
-    stack.add(Visit(startX, startY - 1, Possibles.TOP.letters, 1))
-    stack.add(Visit(startX, startY + 1, Possibles.BOTTOM.letters, 1))
-    stack.add(Visit(startX - 1, startY, Possibles.LEFT.letters, 1))
-    stack.add(Visit(startX + 1, startY, Possibles.RIGHT.letters, 1))
+    stack.add(Visit(startX, startY - 1, Possibles.TOP.letters))
+    stack.add(Visit(startX, startY + 1, Possibles.BOTTOM.letters))
+    stack.add(Visit(startX - 1, startY, Possibles.LEFT.letters))
+    stack.add(Visit(startX + 1, startY, Possibles.RIGHT.letters))
     while (stack.isNotEmpty()) {
         val visit = stack.removeLast()
         if (visit.y < 0 || visit.y >= table.size) continue
         if (visit.x < 0 || visit.x >= table[visit.y].size) continue
 
-        if (table[visit.y][visit.x] == 'S' && visit.step > 2) {
-            println("Found 'S' in ${visit.step} steps")
-            return visit.step / 2
+        if (table[visit.y][visit.x] == 'S' && visited.size > 2) {
+            println("Found 'S' in ${visited.size + 1} steps")
+            visited.add(Point(visit.x, visit.y))
+            return visited
         }
         if (!visit.possibles.contains(table[visit.y][visit.x])) continue
         if (visited.contains(Point(visit.x, visit.y))) continue
@@ -67,32 +70,31 @@ fun part1(input: List<String>): Int {
         val x = visit.x
         val y = visit.y
         val tmp = table[y][x]
-        val step = visit.step
         visited.add(Point(visit.x, visit.y))
         when (tmp) {
             '-' -> { // explore left + right
-                stack.add(Visit(x - 1, y, Possibles.LEFT.letters, step + 1))
-                stack.add(Visit(x + 1, y, Possibles.RIGHT.letters, step + 1))
+                stack.add(Visit(x - 1, y, Possibles.LEFT.letters))
+                stack.add(Visit(x + 1, y, Possibles.RIGHT.letters))
             }
             '|' -> { // explore top + bottom
-                stack.add(Visit(x, y - 1, Possibles.TOP.letters, step + 1))
-                stack.add(Visit(x, y + 1, Possibles.BOTTOM.letters, step + 1))
+                stack.add(Visit(x, y - 1, Possibles.TOP.letters))
+                stack.add(Visit(x, y + 1, Possibles.BOTTOM.letters))
             }
             'F' -> { // explore bottom + right
-                stack.add(Visit(x, y + 1, Possibles.BOTTOM.letters, step + 1))
-                stack.add(Visit(x  + 1, y, Possibles.RIGHT.letters, step + 1))
+                stack.add(Visit(x, y + 1, Possibles.BOTTOM.letters))
+                stack.add(Visit(x  + 1, y, Possibles.RIGHT.letters))
             }
             '7' -> { // explore left + bottom
-                stack.add(Visit(x - 1, y, Possibles.LEFT.letters, step + 1))
-                stack.add(Visit( x, y + 1, Possibles.BOTTOM.letters, step + 1))
+                stack.add(Visit(x - 1, y, Possibles.LEFT.letters))
+                stack.add(Visit( x, y + 1, Possibles.BOTTOM.letters))
             }
             'J' -> { // explore top + left
-                stack.add(Visit(x, y - 1, Possibles.TOP.letters, step + 1))
-                stack.add(Visit(x - 1, y, Possibles.LEFT.letters, step + 1))
+                stack.add(Visit(x, y - 1, Possibles.TOP.letters))
+                stack.add(Visit(x - 1, y, Possibles.LEFT.letters))
             }
             'L' -> { // explore top + right
-                stack.add(Visit(x, y - 1, Possibles.TOP.letters, step + 1))
-                stack.add(Visit(x + 1, y, Possibles.RIGHT.letters, step + 1))
+                stack.add(Visit(x, y - 1, Possibles.TOP.letters))
+                stack.add(Visit(x + 1, y, Possibles.RIGHT.letters))
             }
             else -> throw IllegalStateException("Invalid char -> $tmp")
         }
@@ -100,9 +102,41 @@ fun part1(input: List<String>): Int {
     throw IllegalStateException("No solution found")
 }
 
-fun part2(input: List<String>): Int {
+fun part1(input: List<String>): Int {
+    val table = Array(input.size) { idx -> input[idx].toCharArray() }
+    val (startX, startY) = findStart(table)
 
-    return 2
+    return dfs(table, startX, startY).size / 2
+}
+
+fun part2(input: List<String>): Int {
+    val table = Array(input.size) { idx -> input[idx].toCharArray() }
+    val (startX, startY) = findStart(table)
+    val loopPoints = dfs(table, startX, startY)
+
+    // Clean the map from pipes not in the loop
+    for (y in table.indices) {
+        for (x in table[y].indices) {
+            if (!loopPoints.contains(Point(x, y))) {
+                table[y][x] = '.'
+            }
+        }
+    }
+
+    // Go through the map from left to right counting the number of points that are inside
+    // https://en.wikipedia.org/wiki/Point_in_polygon
+    var insideCount = 0
+    for (y in table.indices) {
+        var inside = false
+        for (x in table[y].indices) {
+            if (setOf('|', 'L', 'J').contains(table[y][x])) {
+                inside = !inside
+            } else if (table[y][x] == '.' && inside) {
+                insideCount += 1
+            }
+        }
+    }
+    return insideCount
 }
 
 fun main() {
@@ -112,5 +146,5 @@ fun main() {
 
     val input = readInput("day10.txt")
     printTimeMillis { print("part1 input = $GREEN" + part1(input) + RESET) }
-    // printTimeMillis { print("part2 input = $GREEN" + part2(input) + RESET) }
+    printTimeMillis { print("part2 input = $GREEN" + part2(input) + RESET) }
 }
